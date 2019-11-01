dff72916ef8c8bec499354647464a258bdda6ec3
Rahul Shah <rahulshah7@outlook.com>
2019-11-01 13:02:56 +0000

    Complete exercise 6

--- fso-part1-unicafe/src/index.js
+++ fso-part1-unicafe/src/index.js
@@ -1,15 +1,42 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const giveGood = () => setGood(good + 1)
+  const giveNeutral = () => setNeutral(neutral + 1)
+  const giveBad = () => setBad(bad + 1)
+
   return (
     <div>
-      code here
+      <h1>Give Feedback</h1>
+      <Button handleClick={giveGood} text='good'></Button>
+      <Button handleClick={giveNeutral} text='neutral'></Button>
+      <Button handleClick={giveBad} text='bad'></Button>
+
+      <h2>Statistics</h2>
+      <table>
+        <tbody>
+          <tr>
+            <td>Good</td>
+            <td>{good}</td>
+          </tr>
+          <tr>
+            <td>Neutral</td>
+            <td>{neutral}</td>
+          </tr>
+          <tr>
+            <td>Bad</td>
+            <td>{bad}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
